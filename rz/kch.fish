6ae5c7669369390157896ed9e7b2a632d749453c
ixingz <122230255+ixingz@users.noreply.github.com>
2024-02-01 12:26:36 +0000

    .

---
+++ rz/kch.fish
@@ -0,0 +1,10 @@
+#!/bin/fish
+/workspaces/rz-UBRWWHC/rz/pnp.fish
+/workspaces/rz-UBRWWHC/rz/fixrs.fish
+/workspaces/rz-UBRWWHC/rz/st.sh
+/workspaces/rz-UBRWWHC/rz/aryt.fish
+/workspaces/rz-UBRWWHC/rz/tgpt.fish
+/workspaces/rz-UBRWWHC/rz/bun.fish
+/workspaces/rz-UBRWWHC/rz/tmuxx.fish
+
+
